b4563388589cdebe7f4eeefcb59e4d21205ea5e6
ismail <mr.yfam@mail.ru>
2026-06-02 16:16:42 +0300

    two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,17 @@ import { IUserInfo } from './training';
 import { colors } from '../enums/Color';
 import { Collection } from './collection';
 import { IOffer } from '../interfaces/IOffer';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-
 export class AppComponent {
+  locationTour: string = '';
+
   companyName: string = 'румтибет';
   loremIpsum: string = 'Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа Hampden-Sydney, штат Вирджиния, взял одно из самых странных слов в Lorem Ipsum, "consectetur"и занялся его поисками в классической латинской литературе.';
 
