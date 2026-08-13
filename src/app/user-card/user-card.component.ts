a44e57b386c3bdf2de62b3706c670e750dd76e06
ismail <mr.yfam@mail.ru>
2026-08-13 22:12:10 +0300

    add  animated directive

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -2,10 +2,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IUser } from '../../interfaces/IUser';
 import { UpperCasePipe } from '@angular/common';
 import { BoldOnHoverDirective } from '../bold-on-hover.directive';
+import { AnimatedGradientOnHoverDirective } from '../animated-gradient-on-hover.directive';
+import { PhoneFormatPipe } from "../phone-format.pipe";
 
 @Component({
   selector: 'app-user-card',
-  imports: [UpperCasePipe, BoldOnHoverDirective],
+  imports: [UpperCasePipe, BoldOnHoverDirective, AnimatedGradientOnHoverDirective, PhoneFormatPipe],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss',
 })
