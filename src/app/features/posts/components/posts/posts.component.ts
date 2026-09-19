0c2f6e9235c348986cec4ebe4cae737a7d354162
ismail <mr.yfam@mail.ru>
2026-09-19 14:57:07 +0300

    feat(posts): add typed paginated posts table

--- src/app/features/posts/components/posts/posts.component.ts
+++ src/app/features/posts/components/posts/posts.component.ts
@@ -1,21 +1,38 @@
 import { Component, inject } from '@angular/core';
 import { PostsApiService } from '../../services/posts-api.service';
 import { TableModule } from 'primeng/table';
+import { IPost } from '../../models/post.model';
+import { PaginatorModule } from 'primeng/paginator';
+import { PaginatorState } from 'primeng/paginator';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [TableModule],
+  imports: [TableModule, PaginatorModule],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss',
 })
 export class PostsComponent {
   postsApiService = inject(PostsApiService);
-  posts: any;
+  posts: IPost[] = [];
+  first = 0;
+  rows = 10;
+  totalRecords = 0;
 
   ngOnInit() {
-    this.postsApiService.getPosts().subscribe(
-      (post: any) => { this.posts = post.posts; console.log(this.posts) }
-    );
+    this.postsApiService.getPosts(10, 0).subscribe((response) => {
+      this.posts = response.posts;
+      this.totalRecords = response.total;
+      console.log(this.posts)
+    });
+  }
+
+  onPageChange(event: PaginatorState): void {
+    this.first = event.first ?? 0;
+    this.rows = event.rows ?? 10;
+
+    this.postsApiService.getPosts(this.rows, this.first).subscribe((response) => {
+      this.posts = response.posts;
+    })
   }
 }
--- src/app/features/posts/services/posts-api.service.ts
+++ src/app/features/posts/services/posts-api.service.ts
@@ -1,5 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IPostResponse } from '../models/post.model';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +9,9 @@ import { HttpClient } from '@angular/common/http';
 export class PostsApiService {
   http = inject(HttpClient);
 
-  getPosts() {
-    return this.http.get('https://dummyjson.com/posts');
+  getPosts(limit: number, skip: number): Observable<IPostResponse> {
+    return this.http.get<IPostResponse>(
+      `https://dummyjson.com/posts?limit=${limit}&skip=${skip}`
+    );
   }
 }
